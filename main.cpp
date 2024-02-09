c226d84ec02df2fe8ffdf76fab7729846c55c59c
mahdissoori84 <159523107+mahdissoori84@users.noreply.github.com>
2024-02-09 12:50:26 +0330

    Add files via upload
     تکلیف جلسه ی اول گروه نهم

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout<<"input num1 : \n" ;
+  int num1 ;
+  std::cin>>num1 ;
+
+  std::cout<<"input num2: \n" ;
+  int num2 ;
+  std::cin>>num2 ;
+  std::cout<<num1+num2;
+  return 0 ;
+}
