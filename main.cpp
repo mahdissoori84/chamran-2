1c17bce062c504a9e29d0cb73da098588d5f0177
mahdissoori84 <159523107+mahdissoori84@users.noreply.github.com>
2024-02-12 23:41:32 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    //the first way taught in class:
-
-    cout << "****\n" ;
-    cout << "***\n" ;
-    cout << "**\n" ;
-    cout << "*\n" ;
+    //output : Mahdis Soori
+    //77, 97, 104, 100, 105, 115, 83, 111, 111, 114, 105
+    int M, a, h, d, i, s, S, o, r, space;
+    M = 77 , a = 97, h = 104, d = 100 , i = 105 ,s = 115 ;
+    S = 83,o = 111,o = 111 ,r = 114,i = 105 ;
+    cout << char(M) << char(a) << char(h) << char(d) << char(i) << char(s) << end 1 ;
+    cout << char(S) << char(o) << char(o) << char(r) << char(i) ;
     return 0;
 }
